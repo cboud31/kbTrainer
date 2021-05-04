7932283d3f30fe8937431485aeab24a331539ff4
Cory Boudreaux <cboud31@gmail.com>
2021-05-03 20:31:25 -0500

    create-react-app installation

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Material U-I imports
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1>The NavBar goes here.</h1>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavBar } from '../../components/';
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Home;
+
+// This is the Home page. It is the first page users see when they visit the site.
+// The Home page will be made of 3 main components: Menu, Header, and Footer.
